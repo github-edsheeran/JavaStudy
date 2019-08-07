ac31abae4519390a241e3c457724e4485b9a7c87
liudongman <liudongman265@gmail.com>
2019-08-07 17:18:21 +0800

    加个测试类，避免git同步出问题

---
+++ src/SXT/JavaPrimaryStage/part1/Test.java
@@ -0,0 +1,11 @@
+package SXT.JavaPrimaryStage.part1;
+
+/**
+ * @program: JavaStudy
+ * @description:
+ * @chineseDescription:
+ * @author: LiuDongMan
+ * @createdDate: 2019-08-07 17:17
+ **/
+public class Test {
+}
