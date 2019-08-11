e3c16585383cac0c2828836292b8fb51d3149c02
liudongman <liudongman265@gmail.com>
2019-08-11 22:22:04 +0800

    完善继承中，构造器、静态初始化块以及初始化块的调用顺序的例子

--- src/SXT/_2BaseOfObjectOriented/part1/InheritAndStaticBlock.java
+++ src/SXT/_2BaseOfObjectOriented/part1/InheritAndStaticBlock.java
@@ -3,13 +3,23 @@ package SXT._2BaseOfObjectOriented.part1;
 /**
  * @program: JavaStudy
  * @description:
- * @chineseDescription: 简单理解继承中，构造器以及静态初始化块的调用顺序
+ * @chineseDescription: 简单理解继承中，构造器、静态初始化块以及初始化块的调用顺序
  * @author: LiuDongMan
  * @createdDate: 2019-08-09 17:12
  **/
 public class InheritAndStaticBlock {
     public static void main(String[] args) {
-
+        Student student = new Student("张三", 18, "男", "1");
+        /**
+         * 调用Person类静态初始化块
+         * 调用Student类静态初始化块
+         * 调用Person类的初始化块
+         * 调用Person类构造器
+         * 调用Student类初始化块
+         * 调用Student类构造器
+         *
+         * 可以看到，静态初始化块、初始化块、构造器的调用顺序是一样的，都是由父类开始往下
+         */
     }
 }
 
@@ -17,6 +27,10 @@ class Person {
     private String name;
     private int age;
     private String gender;
+
+    {
+        System.out.println("调用Person类的初始化块");
+    }
 
     static {
         System.out.println("调用Person类静态初始化块");
@@ -56,6 +70,10 @@ class Person {
 
 class Student extends Person {
     private String StudentId;
+
+    {
+        System.out.println("调用Student类初始化块");
+    }
 
     static {
         System.out.println("调用Student类静态初始化块");
