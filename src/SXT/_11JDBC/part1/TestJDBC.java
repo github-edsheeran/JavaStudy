a8adb20ef23fbd0e2b73581208ec566e1242daca
liudongman <liudongman265@gmail.com>
2019-09-27 15:30:26 +0800

    修改了之前提交的一些内容

--- src/SXT/_11JDBC/part1/TestJDBC.java
+++ src/SXT/_11JDBC/part1/TestJDBC.java
@@ -14,10 +14,22 @@ import java.sql.SQLException;
 public class TestJDBC {
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "987654321");
+            // 这是加载低版本驱动类的方式，8.0之后的版本需要用新的方式进行加载
+//            Class.forName("com.mysql.jdbc.Driver");
+//            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "123456");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            long startTime = System.currentTimeMillis();
+
+            /**
+             * 8.0 以上版本不需要建立SSL连接的，需要显示关闭
+             * 连接对象内部其实包含了Socket对象，是一个远程连接，比较耗时，后期真正的开发中，为了提高效率，都会使用连接池来管理连接对象
+             */
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false&serverTimezone=UTC","root","123456");
+
+            long endTime = System.currentTimeMillis();
 
             System.out.println(conn);
+            System.out.println("建立连接耗时：" + (endTime - startTime) + "毫秒");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
